da88fb30afc49a5b23d90473abc8262345375fea
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-05-09 15:42:02 +0200

    feat: import middlewares in app for error-management

--- app.js
+++ app.js
@@ -4,10 +4,23 @@ const express = require('express');
 //importo routers
 const movieRouters = require('./routers/movieRouters')
 
+//importo i middleware per la gestiore degli errori
+const errorsHandler = require('./middleware/errorHandler');
+const notFound = require('./middleware/notFound');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/movies', movieRouters)
+//decodifico con il body-parser
+app.use(express.json());
+
+app.use('/movies', movieRouters);
+
+//gestione error: 500
+app.use(errorsHandler);
+
+//gestione error:404
+app.use(notFound);
 
 app.listen(port, () => {
     console.log(`Server in listening on port: ${port}`);
