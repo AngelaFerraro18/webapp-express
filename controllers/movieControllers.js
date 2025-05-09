b69d5488a8a8dda176b68b34a45404dec956bfbc
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-05-09 15:23:44 +0200

    feat: create show controller

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -15,4 +15,24 @@ function index(req, res) {
     })
 }
 
-module.exports = { index };
+
+//show
+function show(req, res) {
+
+    //ricavo l'id per identificare il singolo movie
+    let id = parseInt(req.params.id);
+
+    //query per visualizzare un movie
+    const sql = `SELECT * FROM movies WHERE id= ?`;
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (results === 0) return res.status(404).json({
+            error: 'Movie non trovato!'
+        });
+        res.json(results[0]);
+    })
+
+}
+
+module.exports = { index, show };
