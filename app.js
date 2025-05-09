a08e224f65d20f5ea2397d6abc1d1cc2eff5d636
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-05-09 15:10:34 +0200

    feat: add env.PORT

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 //importo express
 const express = require('express');
 
+//importo routers
+const movieRouters = require('./routers/movieRouters')
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use('/movies', movieRouters)
 
 app.listen(port, () => {
     console.log(`Server in listening on port: ${port}`);
