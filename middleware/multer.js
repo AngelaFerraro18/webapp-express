5a178e92ad965727bddb8bb3f9a4ac428c214399
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-05-15 15:01:56 +0200

    feat: import slugify and set slugifiedName

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,11 +1,18 @@
 const multer = require('multer');
+const slugify = require('slugify');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './public/movies_cover')
     },
     filename: function (req, file, cb) {
-        const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}-${file.originalname}`
+
+        const slugifiedName = slugify(file.originalname, {
+            lower: true,
+            trim: true
+        });
+
+        const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}-${slugifiedName}`
         cb(null, uniqueName);
     }
 })
